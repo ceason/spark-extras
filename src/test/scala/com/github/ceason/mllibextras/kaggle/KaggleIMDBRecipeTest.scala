da8ffb905c786e83c4e784459581ff72e4671d81
Chris Eason <ceason@redventures.com>
2017-03-10 13:49:05 -0500

    wire up for RMSE evaluator

--- src/test/scala/com/github/ceason/mllibextras/kaggle/KaggleIMDBRecipeTest.scala
+++ src/test/scala/com/github/ceason/mllibextras/kaggle/KaggleIMDBRecipeTest.scala
@@ -3,10 +3,13 @@ package com.github.ceason.mllibextras.kaggle
 import com.github.ceason.mllibextras.{LocalSpark, LogLossEvaluator}
 import org.apache.spark.ml.PipelineStage
 import org.apache.spark.ml.classification.RandomForestClassifier
+import org.apache.spark.ml.evaluation.RegressionEvaluator
 import org.apache.spark.ml.feature._
 import org.apache.spark.ml.linalg.{Vector ⇒ MLVector}
 import org.apache.spark.ml.tuning.ParamGridBuilder
+import org.apache.spark.mllib.evaluation.RegressionMetrics
 import org.apache.spark.sql.DataFrame
+import org.apache.spark.sql.functions._
 import org.scalatest.FlatSpec
 
 /**
@@ -19,32 +22,33 @@ class KaggleIMDBRecipeTest extends FlatSpec with LocalSpark {
 
 	val totallyRandomSeed = 4
 
-	val labelCol = "default_oct_probability"
+	val labelCol = "imdb_score_x10"
 
 	val trainData: DataFrame = {
-		val d = spark.read
+		spark.read
 			.option("nullValue", "NA")
 			.option("header", "true")
 			.option("inferSchema", true)
-			.csv("src/main/resources/train.csv")
-		new StringIndexer()
-			.setInputCol("default_oct")
-			.setOutputCol(labelCol)
-			.fit(d)
-			.transform(d)
+			.csv("src/main/resources/imdb_train_public.csv")
+			.na.fill(0)
+			.withColumn(labelCol, expr("imdb_score * 10")) // make it a whole number
 	}.cache()
+
+	//	trainData.printSchema()
 
 	val testData: DataFrame = spark.read
 		.option("nullValue", "NA")
 		.option("header", "true")
 		.option("inferSchema", true)
-		.csv("src/main/resources/test.csv")
+		.csv("src/main/resources/imdb_test_public.csv")
+		.na.fill(0)
 		.cache()
 
-	val evaluatorCol = "evalcol"
-	val evaluator: LogLossEvaluator = new LogLossEvaluator()
+	val evaluatorCol = "prediction"
+	val evaluator = new RegressionEvaluator()
+		.setMetricName("rmse")
+		.setPredictionCol(evaluatorCol)
 		.setLabelCol(labelCol)
-		.setProbabilityCol(evaluatorCol)
 
 	val recipeTemplate: (String, ParamGridBuilder, Seq[PipelineStage]) ⇒ KaggleRecipe = {
 		KaggleRecipe(
@@ -66,44 +70,46 @@ class KaggleIMDBRecipeTest extends FlatSpec with LocalSpark {
 		.setSeed(totallyRandomSeed)
 		.setFeaturesCol("selectedFeatures")
 		.setLabelCol(labelCol)
-		.setPredictionCol("prediction")
+		.setPredictionCol(evaluatorCol)
 
-	val recipe1: KaggleRecipe = recipeTemplate("someTestRecipe",
+	val numericCols: List[String] = List("actor_1_facebook_likes",
+		"actor_2_facebook_likes",
+		"actor_3_facebook_likes",
+		"budget",
+		"cast_total_facebook_likes",
+		"director_facebook_likes",
+		"duration",
+		"facenumber_in_poster",
+		"gross",
+		"movie_facebook_likes",
+		"num_critic_for_reviews",
+		"num_user_for_reviews",
+		"num_voted_users",
+		"title_year",
+		"aspect_ratio")
+
+	val stringCols: List[String] = List("actor_1_name",
+		"actor_2_name",
+		"actor_3_name",
+		"color",
+		"content_rating",
+		"country",
+		"director_name",
+		"genres",
+		"language",
+		"movie_imdb_link",
+		"movie_title",
+		"plot_keywords")
+
+	val recipe1: KaggleRecipe = recipeTemplate("imdb_rf",
 		new ParamGridBuilder()
-//			.addGrid(rf.numTrees, 15 to 75 by 15)
-//			.addGrid(rf.featureSubsetStrategy, Seq("onethird", "sqrt", "log2"))
-//			.addGrid(rf.maxDepth, 3 to 9 by 2)
+			//			.addGrid(rf.numTrees, 15 to 75 by 15)
+			.addGrid(rf.featureSubsetStrategy, Seq("onethird", "sqrt", "log2"))
+			//			.addGrid(rf.maxDepth, 3 to 9 by 2)
 			.addGrid(rf.impurity, Seq("entropy", "gini")),
 		List(
-			new SQLTransformer().setStatement(
-				s"""select
-	   				customer_id,
-					coalesce(cast(pay_1     as double), 0.0) as pay_1,
-					coalesce(cast(pay_2     as double), 0.0) as pay_2,
-					coalesce(cast(pay_3     as double), 0.0) as pay_3,
-					coalesce(cast(pay_4     as double), 0.0) as pay_4,
-					coalesce(cast(pay_5     as double), 0.0) as pay_5,
-					coalesce(cast(pay_6     as double), 0.0) as pay_6,
-					coalesce(cast(bill_amt1 as double), 0.0) as bill_amt1,
-					coalesce(cast(bill_amt2 as double), 0.0) as bill_amt2,
-					coalesce(cast(bill_amt3 as double), 0.0) as bill_amt3,
-					coalesce(cast(bill_amt4 as double), 0.0) as bill_amt4,
-					coalesce(cast(bill_amt5 as double), 0.0) as bill_amt5,
-					coalesce(cast(bill_amt6 as double), 0.0) as bill_amt6,
-					coalesce(cast(pay_amt1  as double), 0.0) as pay_amt1,
-					coalesce(cast(pay_amt2  as double), 0.0) as pay_amt2,
-					coalesce(cast(pay_amt3  as double), 0.0) as pay_amt3,
-					coalesce(cast(pay_amt4  as double), 0.0) as pay_amt4,
-					coalesce(cast(pay_amt5  as double), 0.0) as pay_amt5,
-					coalesce(cast(pay_amt6  as double), 0.0) as pay_amt6,
-					coalesce(cast(limit_bal as double), 0.0) as limit_bal,
-	 				$labelCol
-			 	from __THIS__"""),
 			new VectorAssembler()
-				.setInputCols(Array("pay_1", "pay_2", "pay_3", "pay_4", "pay_5", "pay_6",
-					"bill_amt1", "bill_amt2", "bill_amt3", "bill_amt4", "bill_amt5", "bill_amt6",
-					"pay_amt1", "pay_amt2", "pay_amt3", "pay_amt4", "pay_amt5", "pay_amt6",
-					"limit_bal"))
+				.setInputCols(numericCols.toArray)
 				.setOutputCol("features"),
 			new VectorIndexer()
 				.setInputCol("features")
@@ -114,15 +120,13 @@ class KaggleIMDBRecipeTest extends FlatSpec with LocalSpark {
 				.setFeaturesCol("indexedFeatures")
 				.setLabelCol(labelCol)
 				.setOutputCol("selectedFeatures"),
-			rf,
-			new SQLTransformer()
-				.setStatement(s"select *, vec2arr(probability)[1] as $evaluatorCol from __THIS__")
+			rf
 		))
 
 	val start = System.currentTimeMillis
 
-	//	recipe1.transformedData.printSchema()
-//		recipe1.transformedData.show()
+	recipe1.transformedData.printSchema()
+	recipe1.transformedData.show(false)
 	recipe1.writeCsv()
 
 	val duration = (System.currentTimeMillis - start) / 1000d
